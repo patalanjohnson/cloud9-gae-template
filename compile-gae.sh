a87054237295f09782055a90b13e7cf44435aa6a
patalanjohnson <patalanjohnson@gmail.com>
2016-04-18 23:05:47 -0700

    Update to latest versions of Python and GAE SDK

--- compile-gae.sh
+++ compile-gae.sh
@@ -1,18 +1,18 @@
 function install_python27 {
-    wget http://www.python.org/ftp/python/2.7.3/Python-2.7.3.tgz
-    tar xvfz Python-2.7.3.tgz
-    cd Python-2.7.3 
+    wget http://www.python.org/ftp/python/2.7.11/Python-2.7.11.tgz
+    tar xvfz Python-2.7.11.tgz
+    cd Python-2.7.11
     ./configure --prefix=$HOME
     make
     make install
     cd ..
-    rm -rf Python-2.7.3*
+    rm -rf Python-2.7.11*
 }
 
 function install_appengine {
-    wget http://googleappengine.googlecode.com/files/google_appengine_1.7.2.zip
-    unzip google_appengine_1.7.2.zip
-    rm google_appengine_1.7.2.zip
+    wget https://storage.googleapis.com/appengine-sdks/featured/google_appengine_1.9.36.zip
+    unzip google_appengine_1.9.36.zip
+    rm google_appengine_1.9.36.zip
     mv google_appengine ../lib/
     cd ../bin/
     ln -s ../lib/google_appengine/*.py .
